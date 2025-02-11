60df74d6d4bd3dad70264222a6b131dd95e239a6
Sandro Wüthrich <aleksi.w@protonmail.com>
2025-02-11 13:56:41 +0200

    lainauksen tallennus

--- lainaaja_ui_functionality.py
+++ lainaaja_ui_functionality.py
@@ -1,9 +1,13 @@
 import os
 import sys
+import datetime 
+import json
 
 from PySide6 import QtWidgets
 
 from Lainaaja import Ui_MainWindow
+import dbOperations
+import cipher
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -14,14 +18,21 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         # Kutsutaan käyttöliittymän muodostusmetodia seupUi
         self.ui.setupUi(self)
 
-        # Ohjelman käynnistyksessä piilotetaan tarpeettomat elementit
-        self.ui.ajokorttiLineEdit.hide()
-        self.ui.hetuLabel.hide()
-        self.ui.avainLineEdit.hide()
-        self.ui.rekisteriNrLabel.hide()
-        self.ui.naytaTiedotPushButton.hide()
-        self.ui.tilaLabel.setText('VALITSE TOIMINTO')
+        try: 
+            with open('settings.json', 'rt') as settingsFile:
+                jsonData = settingsFile.read()
+                self.currentSettings = json.loads(jsonData)
+            self.plainTextPassword = cipher.decryptString(self.currentSettings['password'])
+        except Exception as error:
+            title = 'Asetustiedosto saattaa puuttua!'
+            text = 'Tietokannan asetusten luku ein onnistunut'
+            detailedText = str(error)
+            self.openWarning(title, text, detailedText)
 
+        self.setInitialElements()
+
+        # Ohjelman käynnistyksessä piilotetaan tarpeettomat elementit
+        
         # OHJELMOIDUT SIGNAALIT
 
         # Kun Tulosta-painiketta on klikattu, kutsutaan updatePrintedLabel metodi
@@ -31,19 +42,49 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.avainLineEdit.returnPressed.connect(self.tilaValmis)
         self.ui.palautaPushButton.clicked.connect(self.activateReturn)
         self.ui.naytaTiedotPushButton.clicked.connect(self.tiedot)
+        self.ui.alkuunPushButton.clicked.connect(self.startView)
+        self.ui.okPushButton.clicked.connect(self.saveLendingData)
 
         # OHJELMOIDUT SLOTIT
 
+    def setInitialElements(self):
+        self.ui.ajokorttiLineEdit.hide()
+        self.ui.hetuLabel.hide()
+        self.ui.avainLineEdit.hide()
+        self.ui.rekisteriNrLabel.hide()
+        self.ui.naytaTiedotPushButton.hide()
+        self.ui.okPushButton.hide()
+        self.ui.alkuunPushButton.hide()
+        self.ui.alkuLabel.hide()
+        self.ui.paattyminenLabel.hide()
+        self.ui.tilaLabel.setText('VALITSE TOIMINTO')
+
+    def startView(self):
+        self.ui.lainaaPushButton.show()
+        self.ui.palautaPushButton.show()
+        self.ui.ajokorttiLineEdit.hide()
+        self.ui.ajokorttiLineEdit.setText('')
+        self.ui.hetuLabel.hide()
+        self.ui.avainLineEdit.hide()
+        self.ui.avainLineEdit.setText('')
+        self.ui.rekisteriNrLabel.hide()
+        self.ui.naytaTiedotPushButton.hide()
+        self.ui.okPushButton.hide()
+        self.ui.alkuunPushButton.hide()
+        self.ui.alkuLabel.hide()
+        self.ui.paattyminenLabel.hide()
+        self.ui.tilaLabel.setText('VALITSE TOIMINTO')
+
     def activateLend(self):
-        self.ui.tilaLabel.setText('AUTON LAINAUS')
+        self.ui.tilaLabel.setText('AUTON LAINAUS') 
         self.ui.ajokorttiLineEdit.show()
+        self.ui.alkuunPushButton.show()
         self.ui.ajokorttiLineEdit.setFocus()
         self.ui.hetuLabel.hide()
         self.ui.statusbar.showMessage('Skannaa ajokortin!')
         self.ui.palautaPushButton.hide()
         self.ui.lainaaPushButton.hide()
         
-
     def activateAvain(self):
         self.ui.ajokorttiLineEdit.hide()
         self.ui.hetuLabel.hide()
@@ -55,11 +96,13 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.avainLineEdit.hide()
         self.ui.rekisteriNrLabel.hide()
         self.ui.naytaTiedotPushButton.show()
+        self.ui.okPushButton.show()
         self.ui.tilaLabel.setText('VALMIS')
 
     def activateReturn(self):
         self.ui.tilaLabel.setText('AUTON PALAUTUS')
         self.ui.avainLineEdit.show()
+        self.ui.alkuunPushButton.show()
         self.ui.avainLineEdit.setFocus()
         self.ui.rekisteriNrLabel.show()
         self.ui.lainaaPushButton.hide()
@@ -68,7 +111,41 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def tiedot(self):
         self.ui.hetuLabel.show() 
         self.ui.rekisteriNrLabel.show()
+        self.ui.alkuLabel.show()
+        self.ui.alkuLabel.setText(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+        print(datetime.datetime.now())
+        self.ui.paattyminenLabel.show()
         self.ui.naytaTiedotPushButton.hide()
+
+    def saveLendingData(self):
+        dbSettings = self.currentSettings
+        plainTextPassword = self.plainTextPassword
+        dbSettings['Password'] = plainTextPassword
+
+        try:
+            dbConnection = dbOperations.DbConnection(dbSettings)
+            ssn = self.ui.hetuLabel.text()
+            key = self.ui.rekisteriNrLabel.text()
+            dataDictionary = {'hetu': ssn,
+                              'rekisterinumero': key}
+            dbConnection.addToTable('lainaus', dataDictionary)
+            
+            self.startView()
+            self.ui.statusbar.showMessage('Auton lainaustiedot tallenettiin', 5000)
+        except Exception as e:
+            title = 'Lainaustietojen tallentaminen ei onnistunut'
+            text = 'Ajokortin tai rekisterinumertiedot virheelliset! Ota yhteyttä hekilökuntaan.'
+            detailedText = str(e)
+            self.openWarning(title, text, detailedText)
+
+    def openWarning(self, title: str, text: str, detailedText: str) -> None:
+        msgBox = QtWidgets.QMessageBox()
+        msgBox.setIcon(QtWidgets.QMessageBox.Critical)
+        msgBox.setWindowTitle(title)
+        msgBox.setText(text)
+        msgBox.setDetailedText(detailedText)
+        msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msgBox.exec()
 # LUODAAN VARSINAINEN SOVELLUS
 
 app = QtWidgets.QApplication(sys.argv)
@@ -79,3 +156,5 @@ windows.show()
 
 # Käynnistetään sovellus ja tapahtumienkäsittelijä (event loop)
 app.exec()
+
+
